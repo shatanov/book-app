b0b957f1df496de6307bc7382baf32e801543736
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-11-23 00:15:32 +0300

    save token to LocalStorage

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -5,6 +5,7 @@ import { catchError, switchMap, map, of } from 'rxjs';
 import { registerAction, registerFailureAction, registerSuccsesAction } from '../actions/register.action';
 import { AuthService } from '../../services/auth.service';
 import { CurrentUserInterface } from '../../../shared/types/currentUser.interface';
+import { PersistanceService } from '../../../shared/services/persistance.service';
 
 @Injectable()
 export class RegisterEffect {
@@ -13,6 +14,7 @@ export class RegisterEffect {
     switchMap(({ request }) => {
       return this.authService.register(request).pipe(
         map((currentUser: CurrentUserInterface) => {
+          this.persistanceService.set("tokenName", currentUser.fullName) // Используем имя за место JWT токена
           return registerSuccsesAction({ currentUser });
         }),
         catchError(() => {
@@ -21,6 +23,8 @@ export class RegisterEffect {
       );
     })
   ));
- // TODO catchError don`t work
-  constructor(private actions$: Actions, private authService: AuthService) {}
+
+  // TODO catchError don`t work
+  constructor(private actions$: Actions, private authService: AuthService,
+              private persistanceService: PersistanceService) {}
 }

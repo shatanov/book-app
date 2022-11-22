31c08864f3fb39b97fae7fd0e0a77ef6f2fd3c3a
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-11-23 00:27:02 +0300

    add redirect effect

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, switchMap, map, of } from 'rxjs';
+import { catchError, switchMap, map, of, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 import { registerAction, registerFailureAction, registerSuccsesAction } from '../actions/register.action';
 import { AuthService } from '../../services/auth.service';
 import { CurrentUserInterface } from '../../../shared/types/currentUser.interface';
 import { PersistanceService } from '../../../shared/services/persistance.service';
+
 
 @Injectable()
 export class RegisterEffect {
@@ -14,7 +16,7 @@ export class RegisterEffect {
     switchMap(({ request }) => {
       return this.authService.register(request).pipe(
         map((currentUser: CurrentUserInterface) => {
-          this.persistanceService.set("tokenName", currentUser.fullName) // Используем имя за место JWT токена
+          this.persistanceService.set('tokenName', currentUser.fullName); // Используем имя за место JWT токена
           return registerSuccsesAction({ currentUser });
         }),
         catchError(() => {
@@ -24,7 +26,17 @@ export class RegisterEffect {
     })
   ));
 
+  redirectAfterSubmit$ = createEffect(() => this.actions$.pipe(
+      ofType(registerSuccsesAction),
+      tap(() => {
+        this.router.navigateByUrl('/');
+      })
+    ),
+    {dispatch: false}
+  );
+
   // TODO catchError don`t work
   constructor(private actions$: Actions, private authService: AuthService,
-              private persistanceService: PersistanceService) {}
+              private persistanceService: PersistanceService, private router: Router) {
+  }
 }

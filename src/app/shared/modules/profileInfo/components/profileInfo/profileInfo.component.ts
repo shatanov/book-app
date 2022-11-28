51cfe168c5f01201505492f809d9bd1d22ca4ed5
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-11-29 01:18:35 +0300

    fix profileInfo module

--- src/app/shared/modules/profileInfo/components/profileInfo/profileInfo.component.ts
+++ src/app/shared/modules/profileInfo/components/profileInfo/profileInfo.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { select, Store } from '@ngrx/store';
+
+import { CurrentUserInterface } from '../../../../types/currentUser.interface';
+import { currentUserSelector, isLoggedInSelector } from '../../../../../auth/store/selectors';
 
 @Component({
   selector: 'app-profile-info',
   templateUrl: './profileInfo.component.html',
   styleUrls: ['./profileInfo.component.scss']
 })
-export class ProfileInfoComponent {
+export class ProfileInfoComponent implements OnInit{
+  isLoggedIn$: Observable<boolean | null>
+  currentUser$: Observable<CurrentUserInterface | null>
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    // @ts-ignore
+    this.isLoggedIn$ = this.store.pipe(select(isLoggedInSelector));
+    // @ts-ignore
+    this.currentUser$ = this.store.pipe(select(currentUserSelector));
+  }
 }

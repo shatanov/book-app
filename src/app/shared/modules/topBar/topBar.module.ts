22623b64d9196c937316f4179ba92cea858c2fcf
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-11-29 01:40:55 +0300

    add searchInput component

--- src/app/shared/modules/topBar/topBar.module.ts
+++ src/app/shared/modules/topBar/topBar.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLinkWithHref } from '@angular/router';
 
 import { TopBarComponent } from './components/topBar/topBar.component';
 import { MainLogoModule } from '../mainLogo/mainLogo.module';
 import { ProfileInfoModule } from '../profileInfo/profileInfo.module';
-import { RouterLinkWithHref } from '@angular/router';
+import { SearchInputComponent } from './components/searchInput/searchInput.component';
 
 @NgModule({
   imports: [CommonModule, MainLogoModule, ProfileInfoModule, RouterLinkWithHref],
-  declarations: [TopBarComponent],
+  declarations: [TopBarComponent, SearchInputComponent],
   exports: [TopBarComponent]
 })
 export class TopBarModule {}

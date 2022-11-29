3803aca58931ef459ffae962b31147ac896b6bf4
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-11-29 23:20:44 +0300

    add mainPage component

---
+++ src/app/main/components/mainPage/mainPage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './mainPage.component.html',
+  styleUrls: ['./mainPage.component.scss']
+})
+
+export class MainPageComponent {
+
+}

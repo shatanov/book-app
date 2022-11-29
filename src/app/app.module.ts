90ab7594d0410f544a62f03d8c81a7dc01bd2b02
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-11-29 22:21:01 +0300

    add authinterseptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
 
+import { TopBarModule } from './shared/modules/topBar/topBar.module';
+import { MainPageModule } from './main/main.module';
+import { PersistanceService } from './shared/services/persistance.service';
+import { AuthInterseptor } from './shared/services/authinterseptor.service';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AppComponent } from 'src/app/app.component';
 import { AuthModule } from 'src/app/auth/auth.module';
 import { environment } from '../environments/environment';
-import { HttpClientModule } from '@angular/common/http';
-import { EffectsModule } from '@ngrx/effects';
-import { TopBarModule } from './shared/modules/topBar/topBar.module';
 
 
 @NgModule({
@@ -28,9 +31,16 @@ import { TopBarModule } from './shared/modules/topBar/topBar.module';
       logOnly: environment.production,
       autoPause: true
     }),
-    TopBarModule
+    TopBarModule,
+    MainPageModule
   ],
-  providers: [],
+  providers: [PersistanceService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterseptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {

325bf2f3cfaf3ccd3d02d44e18311c76c54f0157
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-12-01 02:03:30 +0300

    update css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { getCurrentUserAction } from './auth/store/actions/getCurrentUser.action
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
   constructor(private store: Store){}

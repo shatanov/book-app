b7456c010d45045e1d47b69f500eb8cd28951398
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-12-01 02:02:54 +0300

    Update bookFootnote.component.ts

--- src/app/shared/modules/footnote/components/bookFootnote.component.ts
+++ src/app/shared/modules/footnote/components/bookFootnote.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-book-footnote',
+  selector: 'app-bookSet-footnote',
   templateUrl: './bookFootnote.component.html',
   styleUrls: ['./bookFootnote.component.scss']
 })

69071b953e213f8af5350f1f9d3b9ea1dbd44a9f
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-11-30 18:28:46 +0300

    Update book.module.ts

--- src/app/shared/modules/book/book.module.ts
+++ src/app/shared/modules/book/book.module.ts
@@ -6,11 +6,19 @@ import { BookImgComponent } from './componets/bookImg/bookImg.component';
 import { BookInfoComponent } from './componets/bookInfo/bookInfo.component';
 import { BookFootnoteModule } from '../footnote/bookFootnote.module';
 import { GetBookEffect } from './store/effects/getBook.effect';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './store/reducers';
+import { BookService } from './service/book.service';
 
 
 @NgModule({
-  imports: [CommonModule, BookFootnoteModule, EffectsModule.forFeature([GetBookEffect])],
+  imports: [CommonModule,
+    BookFootnoteModule,
+    EffectsModule.forFeature([GetBookEffect]),
+    StoreModule.forFeature('feat', reducer)
+  ],
   declarations: [BookImgComponent, BookInfoComponent],
-  exports: [BookImgComponent, BookInfoComponent]
+  exports: [BookImgComponent, BookInfoComponent],
+  providers: [BookService]
 })
 export class BookModule {}

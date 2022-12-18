7c8569eb6a2c04c8c4adb2a0c233864bc0faef2d
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-12-18 17:54:50 +0300

    Update user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,25 +1,22 @@
-import { IUserAuth } from "../types/userAuth.interface";
 import { IUser } from "../types/user.interface";
 
 export const userService = {
     registerUser,
-    loginUser
+    loginUser,
 };
 const url = " http://localhost:3000";
 
-function registerUser(user: IUserAuth) {
+function registerUser(user: IUser) {
     const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({user})
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user),
     };
     return fetch(`${url}/users`, requestOptions);
 }
 
-function loginUser(loginUser: IUserAuth) {
-    return fetch(`${url}/loginUser`)
-    .then(response => response.json())
-    .then(data => {
-        return data.find((user: IUser) => user.email === loginUser.email && user.password === loginUser.password);
-    });
+function loginUser(loginUser: IUser) {
+    return fetch(`${url}/users`)
+        .then((response) => response.json())
+        .then(data => data.find((user:IUser) => user.email === loginUser.email && user.password === loginUser.password))
 }

43f51c1b9a9de2da49fc2488e32c440c6d1c60ee
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-12-19 21:41:59 +0300

    Create BookFootnotes.vue

---
+++ src/components/BookFootnotes.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="book-info__footnotes">
+        <div class="footnote" v-for="footnote in footnotes" :key="footnote.name"
+        :class="{
+            'footnote--addition': !footnote.status === true,
+        }"
+        >{{footnote.name}}</div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    props: {
+        footnotes: {
+            type: Array,
+            required: true
+        }
+    }
+})
+</script>
+
+
+<style scoped lang="scss">
+@import "../styles/main.scss";
+.footnote {
+    font-weight: var(--text-bold_m);
+    font-size: var(--font-size-m);
+    line-height: 27px;
+    color: var(--dark-grey);
+
+    &--sm {
+        font-size: var(--font-size-s);
+        line-height: 24px;
+    }
+
+    &--addition {
+        font-size: var(--font-size-s);
+        line-height: 24px;
+        color: var(--contrast);
+    }
+
+    &--addition_sm {
+        font-size: var(--font-size-xs);
+        line-height: 21px;
+        color: var(--contrast);
+    }
+}
+</style>

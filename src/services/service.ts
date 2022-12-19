00d112a6e03c683a07c1f30a82fc096dd1f2339c
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-12-19 23:19:25 +0300

    add getBook service

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,8 +1,17 @@
 export const service = {
     getAllCategories,
+    getBook,
 };
 const url = " http://localhost:3000";
 
+import { IBook } from "../types/book.interface"
+
 function getAllCategories() {
     return fetch(`${url}/categories`).then(response => response.json());
+}
+
+function getBook(id: number) {
+    return fetch(`${url}/books`)
+    .then(response => response.json())
+    .then(data => data.find((book:IBook) => book.id === id))
 }

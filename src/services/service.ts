96972e33e9ac65237b195d87bd0b9490730302c1
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-12-21 16:41:53 +0300

    Update service.ts

--- src/services/service.ts
+++ src/services/service.ts
@@ -4,14 +4,14 @@ export const service = {
 };
 const url = " http://localhost:3000";
 
-import { IBook } from "../types/book.interface"
+import { IBook } from "../types/book.interface";
 
 function getAllCategories() {
-    return fetch(`${url}/categories`).then(response => response.json());
+    return fetch(`${url}/categories`).then((response) => response.json());
 }
 
 function getBook(id: number) {
     return fetch(`${url}/books`)
-    .then(response => response.json())
-    .then(data => data.find((book:IBook) => book.id === id))
+        .then((response) => response.json())
+        .then((data) => data.find((book: IBook) => book.id === id));
 }

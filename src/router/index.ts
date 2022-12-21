b14236b1f42d8027a945de8840ba9c7f9e365a0b
Timur Shatanov <timur-shatanov.work@mail.ru>
2022-12-21 22:11:23 +0300

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,44 +1,36 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import RegisterPage from "../views/auth/RegisterPage.vue"
-import LoginPage from "../views/auth/LoginPage.vue"
-import MainPage from "../views/MainPage.vue"
-import BookDetailedPage from "../views/BookDetailedPage.vue"
+import RegisterPage from "../views/auth/RegisterPage.vue";
+import LoginPage from "../views/auth/LoginPage.vue";
+import MainPage from "../views/MainPage.vue";
+import BookDetailedPage from "../views/BookDetailedPage.vue";
+import FeedbackPage from "../views/FeedbackPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/register",
-    name: "register",
-    component: RegisterPage,
-  },
-  {
-    path: "/",
-    name: "home",
-    component: MainPage,
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: LoginPage,
-  },
-  {
-    path: "/detailed/book/:id",
-    name: "book detailed",
-    component: BookDetailedPage,
-  },
-//   {
-//     path: "/about",
-//     name: "about",
-//     // route level code-splitting
-//     // this generates a separate chunk (about.[hash].js) for this route
-//     // which is lazy-loaded when the route is visited.
-//     component: () =>
-//       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-//   },
+    {
+        path: "/register",
+        name: "register",
+        component: RegisterPage,
+    },
+    {
+        path: "/",
+        name: "home",
+        component: MainPage,
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: LoginPage,
+    },
+    {
+        path: "/detailed/book/:id",
+        name: "book detailed",
+        component: BookDetailedPage,
+    }
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
